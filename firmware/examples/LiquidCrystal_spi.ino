23f1b712b5d04845419b9fb6ffc1ffc369259561
Abdulrahman Alotaiba <abdulrahman.alotaiba@gmail.com>
2015-05-05 18:08:04 -0700

    fixed typos in example filenames, where i2c example was named as spi, and spi example was named as i2c.

--- firmware/examples/LiquidCrystal_spi.ino
+++ firmware/examples/LiquidCrystal_spi.ino
@@ -5,9 +5,8 @@
 
 #include "Adafruit_LiquidCrystal/Adafruit_LiquidCrystal.h"
 
-// TO Connect via I2C. Data pin is D0, Clock is D1
-// By default, no jumpers are soldered, giving an address of 0.
-LiquidCrystal lcd(0);
+// TO connect via SPI. Data pin is A5, Clock is A3 and Latch is A2
+LiquidCrystal lcd(A5, A3, A2);
 
 void setup() {
   // set up the LCD's number of rows and columns: 
